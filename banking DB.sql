864073d88f6fb1d2c1f8ff99225289fa0ebaaa9c
MamtaRani <mr6906471@gmail.com>
2026-07-31 21:03:43 +0530

    latest

---
+++ banking DB.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE Banking_DB;
+create table customer(c_id int primary key,_first_name varchar(50), last_name varchar(50), email varchar(100), phone varchar(15));
+insert into customer values(101, "Ajeet", "Mishra", "Ajeet@gmail.com", "1234567893");
+select*from costomer;
+USE Banking_DB;
+SHOW TABLES;
